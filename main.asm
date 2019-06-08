34093e8b00b6ad6999b90fd2e569d865ea5b75be
aoizora <mathesis@inbox.ru>
2019-06-08 16:00:59 +0000

    get linear address of entry

--- main.asm
+++ main.asm
@@ -10,3 +10,14 @@ start:
   in al, 92h
   or al, 2
   out 92h, al
+
+; calculate entry point' linear address
+  xor eax, eax
+  mov ax, cs
+  shl eax, 4
+  add eax, PROTECTED_MODE_ENTRY_POINT
+
+use32
+PROTECTED_MODE_ENTRY_POINT:
+  ; load segment registers
+  nop
